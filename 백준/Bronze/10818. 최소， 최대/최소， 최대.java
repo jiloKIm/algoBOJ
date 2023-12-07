eeb7fbbd55def0f315338c91a80b7aae1b1906ef
jiloKIm <96615422+jiloKIm@users.noreply.github.com>
2023-12-07 18:31:11 +0900

    [Bronze III] Title: 최소, 최대, Time: 1784 ms, Memory: 320352 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < N; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+
+        scanner.close();
+    }
+}
