94df4988c5c16b23b0eddd7c7bcf69f98340a49a
jiloKIm <96615422+jiloKIm@users.noreply.github.com>
2023-12-07 18:32:16 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+
+      char alpha=sc.next().charAt(0);
+      int a=(int)alpha;
+      System.out.println(a);
+}
+}
