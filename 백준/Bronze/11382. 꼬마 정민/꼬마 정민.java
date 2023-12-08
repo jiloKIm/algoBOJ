ab1f1e23a4ef8ba1dd1edd9c1cbe36ca30b4f01d
jiloKIm <96615422+jiloKIm@users.noreply.github.com>
2023-12-08 15:53:03 +0900

    [Bronze V] Title: 꼬마 정민, Time: 200 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // A, B, C 입력받기
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        // A + B + C 계산
+        long result = A + B + C;
+
+        // 결과 출력
+        System.out.println(result);
+    }
+}
