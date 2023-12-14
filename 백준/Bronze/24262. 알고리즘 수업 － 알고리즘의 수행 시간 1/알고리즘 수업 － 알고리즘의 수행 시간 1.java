b83d0d492b6847ee05ed0ce176840dd4c1d4ffea
jiloKIm <96615422+jiloKIm@users.noreply.github.com>
2023-12-14 15:14:14 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  
+        sc.close();
+
+     
+        int executionCount = 1;
+
+     
+        int highestDegree = 0;
+
+        
+        System.out.println(executionCount);
+        System.out.println(highestDegree);
+    }
+}
