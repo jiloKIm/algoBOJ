33cbcf64c840764f5fe7e8aafd60b10a2e795cd4
jiloKIm <96615422+jiloKIm@users.noreply.github.com>
2024-03-07 17:57:52 +0900

    [level 2] Title: 타겟 넘버, Time: 0.41 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,19 @@
-public class Solution {
-    int count = 0; // 타겟 넘버를 만들 수 있는 경우의 수를 세는 변수
+class Solution {
+    int answer = 0;
 
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0); // DFS 시작, 시작 인덱스와 현재 합계는 0
-        return count;
+        dfs(numbers, 0, target, 0);
+
+        return answer;
     }
 
-    public void dfs(int[] numbers, int target, int index, int sum) {
-        if (index == numbers.length) { // 모든 숫자를 다 확인했다면,
-            if (sum == target) { // 현재 합계가 타겟 넘버와 같다면,
-                count++; // 경우의 수를 하나 증가
-            }
-            return; // 리턴하여 재귀 종료
+    // 깊이 우선 탐색
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){ // 마지막 노드 까지 탐색한 경우
+            if(target == sum) answer++;
+        } else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
         }
-
-        // 현재 숫자를 더하는 경우
-        dfs(numbers, target, index + 1, sum + numbers[index]);
-
-        // 현재 숫자를 빼는 경우
-        dfs(numbers, target, index + 1, sum - numbers[index]);
     }
 }
